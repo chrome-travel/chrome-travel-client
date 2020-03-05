55433d7a26cb8f0ca7e7a0d230e2505afc0e5f59
naufalyunan <naufalyunan45@gmail.com>
2020-03-05 22:27:45 +0700

    edit send google token from headers

--- index.js
+++ index.js
@@ -29,15 +29,16 @@ $(document).ready(function () {
 
 function onSignIn(googleUser) {
     const profile = googleUser.getBasicProfile();
-    // console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    // console.log('Name: ' + profile.getName());
-    // console.log('Image URL: ' + profile.getImageUrl());
-    // console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
     const id_token = googleUser.getAuthResponse().id_token;
+    console.log(id_token)
     $.ajax({
         method: "POST",
         url: "http://localhost:3000/loginGoogle",
-        data: {
+        headers: {
             id_token
         }
     })
